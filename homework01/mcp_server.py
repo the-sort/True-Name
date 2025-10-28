19cfa8952f46fb105305d8473efb9534535fd546
Filip Dravecký <dravefil@fit.cvut.cz>
2025-10-28 19:42:05 +0100

    Hw01: added check_availability and validate_district_name functions

--- homework01/mcp_server.py
+++ homework01/mcp_server.py
@@ -3,10 +3,32 @@
 from mcp.server.fastmcp import FastMCP
 from mcp.server.fastmcp.exceptions import FastMCPError
 
-from utils import DISTRICTS_FOLDER, HOST, PORT, TRANSPORT, find_best_path_bfs, NameNotInDistrictError, Node
+from utils import DISTRICTS_FOLDER, HOST, PORT, TRANSPORT, find_best_path_bfs, NameNotInDistrictError, Node, load_district
 
 # Create an MCP server
 server = FastMCP("Path Finder", host=HOST, port=PORT)
+
+def check_availability(district_name):
+    """Checks if resource is available"""
+    try:
+        open(f"{DISTRICTS_FOLDER}/{district_name}.txt", "r", encoding="utf-8")
+        return True
+    except FileNotFoundError:
+        return f"{DISTRICTS_FOLDER}/{district_name}.txt does not exist"
+    except PermissionError:
+        return f"Inssuficient permision to acces {DISTRICTS_FOLDER}/{district_name}.txt"
+
+def validate_district_name(district_name):
+    """ Valid district name contains only:
+        alphanumerical values, spaces, single quotes and underscores"""
+    for char in district_name:
+        if not  (   char.isalnum()  or
+                    char == "'"     or
+                    char == "_"
+                ):
+            return False
+    return True
+
 
 # Prompt
 @server.prompt()
@@ -15,7 +37,14 @@ def sample_prompt():
     return "Find best path from Waffles to Storage in the Maple district."
 
 # Resource
+@server.resource("district://{district_name}")
 def district_resource(district_name):
+    """"Get a desired district resource."""
+    if isinstance(check_availability(district_name), str):
+        return
+    if not validate_district_name(district_name):
+        return
+
     return ...
 
 # Tool
