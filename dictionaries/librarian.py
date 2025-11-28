8b5fb49c59b563abece1f5b43aa31c60d24f4505
Filip Dravecký <dravefil@fit.cvut.cz>
2025-11-28 23:39:49 +0100

    Dict: 	implementetion of helper function to parse and simplify imported 	word dictionaries

---
+++ dictionaries/librarian.py
@@ -0,0 +1,36 @@
+"""
+Procesing downloaded words.txt files
+"""
+from argparse import ArgumentParser
+from os       import remove
+
+parser = ArgumentParser(description = "Process file to game resource")
+
+parser.add_argument("file", help = "file for process")
+parser.add_argument(    "-d", "-D", "--delete",
+                        default = False ,
+                        action  = "store_true",
+                        help    = "delete file after procesing"
+                    )
+args = parser.parse_args()
+new_file_name = str(args.file).removesuffix(".txt") + "_edited.txt"
+
+
+try:
+    with open(args.file, mode = "rt", encoding= "utf-8") as file:
+        with open(new_file_name, mode ="wt+", encoding="utf-8") as new_file:
+            for line in file:
+                separeted = line.split("\t")
+                if len(separeted[0]) <= 2 or not separeted[0].isalpha():
+                    continue
+                if int(separeted[1]) < 15:
+                    break
+                new_file.write(separeted[0]+"\n")
+        if args.delete:
+            remove(args.file)
+            print("File: ", args.file, " was deleted")
+
+except FileNotFoundError:
+    print("File does not exist in this Folder")
+except PermissionError:
+    print("Inssuficient permisions")
