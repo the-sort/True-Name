dc66daf8ac0f45f1e245cc908cffebb26e5aaae5
Filip Dravecký <dravefil@fit.cvut.cz>
2025-12-10 18:21:08 +0100

    Tools: simple percentage calculator

---
+++ tools/utils.py
@@ -0,0 +1,9 @@
+"""
+helper functions
+"""
+
+def percetage(number, percent) -> int:
+        """
+        calculates given percetange of given number
+        """
+        return number * percent / 100
