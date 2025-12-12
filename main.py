c263fe342e30950b7f4def06c8a69d98c47d90e3
Filip Dravecký <dravefil@fit.cvut.cz>
2025-12-12 21:26:53 +0100

    Main,Tool: Visual enhancement

--- main.py
+++ main.py
@@ -4,15 +4,11 @@ This script runs main game_loop
 import pygame
 import scenes.level as level
 
-# 32 x 32
-# 16 x 16
-#  8 x 8
+
 if __name__ == "__main__":
 
     RUNNING  = True
     DELTA_TIME = 0.1
-    SCREEN_W = 1024 # 4 x 3
-    SCREEN_H = 768
 
     pygame.init()
     clock = pygame.time.Clock()
