c06764e20583899f0b9ea2e7d6336492a3a6c100
Filip Dravecký <dravefil@fit.cvut.cz>
2025-12-12 21:43:32 +0100

    Tools: added canvas module

---
+++ tools/canvas.py
@@ -0,0 +1,55 @@
+"""
+Tool for displaying and handling canvas
+"""
+import pygame
+
+class CCanvases:
+    """
+    Class for handling drawing canvas
+    """
+    def __init__(self, screen_height):
+        self.__screen_height = screen_height
+
+        self.__width            = 175
+        self.__height           = 175
+        self.__canvases         = [pygame.Surface((self.__width,self.__height)) for _ in range(10)]
+        self.__canvases_rect    = [canvas.get_rect() for canvas in self.__canvases]
+
+        for canvas in self.__canvases:
+            canvas.fill((255,255,255))
+
+    def display_canvases(self, screen, global_x, global_y):
+        """
+        Metod used for displaying canvases
+        """
+        x_offset = 74
+        for i in range(5):
+            l_corner =  (
+                        x_offset + global_x,
+                        (self.__screen_height//2) + self.__screen_height + global_y - self.__height
+                        )
+
+            screen.blit(self.__canvases[i], l_corner)
+            self.__canvases_rect[i] = self.__canvases[i].get_rect(topleft = l_corner)
+            x_offset += 175
+
+        x_offset = 74
+        for i in range(5,10):
+            l_corner =  (
+                        x_offset + global_x,
+                        (self.__screen_height//2) + self.__screen_height + global_y
+                        )
+            screen.blit(self.__canvases[i], l_corner)
+            self.__canvases_rect[i] = self.__canvases[i].get_rect(topleft = l_corner)
+            x_offset += 175
+    def get_active(self, pos):
+        """
+        Metod that returns canvas with 
+        which player interacts
+        Return None if interacts with nothing
+        Return Surface and rect when found
+        """
+        for i in enumerate(self.__canvases):
+            if self.__canvases_rect[i[0]].collidepoint(pos):
+                return (i[1],self.__canvases_rect[i[0]])
+        return None
