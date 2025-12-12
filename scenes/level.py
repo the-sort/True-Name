52093ef57c859e98141c94d80c241de10b8e8445
Filip Dravecký <dravefil@fit.cvut.cz>
2025-12-12 21:25:58 +0100

    Level: Segment drawing implemented

--- scenes/level.py
+++ scenes/level.py
@@ -8,6 +8,86 @@ import tools.table_generator as tg
 SCREEN_W = 1024 # 4 x 3
 SCREEN_H = 768
 
+class CBrush:
+    """
+    class that represents brush
+    """
+    def __init__(self, color = (255, 0, 0), size = 4):
+        self.__down  = False
+        self.__color = color
+        self.__size  = size
+
+    def up(self):
+        """
+        Lift brush from canvas
+        """
+        self.__down  = False
+
+    def down(self):
+        """
+        Place brush on canvas
+        """
+        self.__down = True
+
+    def is_down(self) -> bool:
+        """
+        Checks if brush is on canvas
+        """
+        return self.__down
+
+    def draw(self, canvas, canvas_rect, pos):
+        """
+        Puts a pixel on canvas
+        """
+        pixel = pygame.Rect(pos[0] - canvas_rect.x , pos[1] - canvas_rect.y, self.__size, self.__size)
+        pygame.draw.rect(canvas, self.__color, pixel)
+
+
+class CCanvases:
+    """
+    Class for handling players drawings
+    """
+    def __init__(self):
+        self.__width            = 175
+        self.__height           = 175
+        self.__canvases         = [pygame.Surface((self.__width,self.__height)) for _ in range(10)]
+        self.__canvases_rect    = [canvas.get_rect() for canvas in self.__canvases]
+
+        for canvas in self.__canvases:
+            canvas.fill((255,255,255))
+    
+    def display_canvases(self, screen, global_x, global_y):
+        """
+        Metod used for displaying canvases
+        """
+        x_offset = 74
+        for i in range(5):
+            l_corner = (x_offset + global_x, (SCREEN_H//2) + SCREEN_H + global_y - self.__height)
+            screen.blit(self.__canvases[i], l_corner)
+            self.__canvases_rect[i] = self.__canvases[i].get_rect(topleft = l_corner)
+            x_offset += 175
+
+        x_offset = 74
+        for i in range(5,10):
+            l_corner = (x_offset + global_x, (SCREEN_H//2) + SCREEN_H + global_y)
+            screen.blit(self.__canvases[i], l_corner)
+            self.__canvases_rect[i] = self.__canvases[i].get_rect(topleft = l_corner)
+            x_offset += 175
+    def get_active(self, pos):
+        """
+        Metod that returns canvas with 
+        which player interacts
+        Return None if interacts with nothing
+        Return Surface and rect when found
+        """
+        for i in enumerate(self.__canvases):
+            if self.__canvases_rect[i[0]].collidepoint(pos):
+                return (i[1],self.__canvases_rect[i[0]])
+        return None
+
+
+
+
 
 class CLevel:
     """
@@ -16,12 +96,16 @@ class CLevel:
     def __init__(self, difficultie, language = "ENG"):
         self.__screen = pygame.display.set_mode((SCREEN_W, SCREEN_H))
         self.__table  = tg.CTable(self.__screen, difficultie, language)
+        # self.__canvas = pygame.Surface((175,175))
 
         self.__x_pos    = 0
         self.__y_pos    = 0
         self.__x_off    = 0
         self.__y_off    = 0
 
+        self.__canvases = CCanvases()
+        self.__brush = CBrush()
+        
     def display_level(self):
         """
         Metod called in game loop
@@ -29,6 +113,10 @@ class CLevel:
 
         self.__screen.fill((0,0,0))
         self.__table.display_table(self.__x_pos, self.__y_pos)
+
+        # self.__screen.blit(self.__canvas, (74 + self.__x_pos, (SCREEN_H//2) + SCREEN_H + self.__y_pos))
+        # self.__canvas_rect = self.__canvas.get_rect(topleft = (74 + self.__x_pos, (SCREEN_H//2) + SCREEN_H + self.__y_pos))
+        self.__canvases.display_canvases(self.__screen, self.__x_pos, self.__y_pos)
 
         center_collider  = pygame.Rect  (   percetage(SCREEN_W, 10) + self.__x_pos            ,
                                             SCREEN_H - percetage(SCREEN_H, 5) + self.__y_pos  ,
@@ -51,6 +139,7 @@ class CLevel:
         pygame.draw.rect(self.__screen, (255,255,255),right_collider)
 
         continue_looping = self.__event_handler(center_collider, left_collider, right_collider)
+
 
         self.__x_pos += self.__x_off
         self.__y_pos += self.__y_off
@@ -75,6 +164,10 @@ class CLevel:
             if event.type == pygame.QUIT:
                 return False
             if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:   # left click
+                    self.__brush.down()
+                    # last_pos = event.pos
+
                 if center_collider.collidepoint(event.pos):
                     if self.__y_pos >= 0:
                         self.__y_off = -8
@@ -90,7 +183,20 @@ class CLevel:
                         self.__x_off = 8
                     else:
                         self.__x_off = -8
+            if event.type == pygame.MOUSEBUTTONUP:
+                if event.button == 1:   # left click
+                    self.__brush.up()
+                    # last_pos = event.pos
+            if event.type == pygame.MOUSEMOTION and self.__brush.is_down():
+                # print(self.__canvas.get_rect().collidepoint)
+                if (canvas := self.__canvases.get_active(event.pos)) is None:
+                    continue
+                self.__brush.draw(canvas[0], canvas[1], event.pos)
+
         return True
+
+    # def __del__(self):
+    #     pygame.image.save(self.__canvas, "Char.png")
 
 
 if __name__ == "__main__":
