fbd6e9b326939e941c498150bde60581a94f97c8
Filip Dravecký <dravefil@fit.cvut.cz>
2025-12-15 11:21:43 +0100

    Tool: code is in PEP-8

--- tools/evaluator.py
+++ tools/evaluator.py
@@ -1,8 +1,14 @@
+"""
+Tool which uses pretrained pytorch
+model to convert user drawn picture
+and compare it to given string
+"""
+
 import torch
 import torchvision.transforms as transforms
 from PIL import Image
-from tools.translator import CSimpleLetterClassifier
 from torchvision.datasets import ImageFolder
+from tools.translator import CSimpleLetterClassifier
 from tools.utils import is_blank
 
 class CEvaluator:
@@ -12,9 +18,10 @@ class CEvaluator:
 
     def __init__(self):
         self.__set_model()
-        self.__preproces_input()
 
-        self.__class_to_index = {v: k for k, v in ImageFolder("dictionaries/letters/train/").class_to_idx.items()}
+        self.__class_to_index = {v: k for k, v in
+                                    ImageFolder("dictionaries/letters/train/").class_to_idx.items()
+                                }
 
 
     def __set_model(self):
@@ -42,21 +49,22 @@ class CEvaluator:
                 continue
             self.__images.append(transform(img).unsqueeze(0))
 
-    def check_answer(self, key = ""):
+    def make_string(self) -> str:
         """
         Checks if users word matches given word
         """
+        self.__preproces_input()
         answer = ""
         for letter in self.__images:
             with torch.no_grad():
                 letter = letter.to(self.__device)
                 prediction = self.__model(letter)
-                print(prediction)
                 probabilities = torch.nn.functional.softmax(prediction, dim = 1)
                 pred_idx = probabilities.argmax(dim=1).item()
                 class_name = self.__class_to_index[pred_idx]
-                answer += class_name
+                answer += class_name.capitalize()
         print(answer)
+        return answer
 
 
 
@@ -65,26 +73,4 @@ class CEvaluator:
 
 if __name__ == "__main__":
     evaluator = CEvaluator()
-    evaluator.check_answer()
-    # device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    # model = CSimpleLetterClassifier().to(device)
-    # model.load_state_dict(torch.load("model.pth", weights_only=True))
-    # model.eval()
-    # for i in range(10):
-    #     img = Image.open(f"profile/input/char{i}.png").convert("L").convert("RGB")
-
-    #     transform = transforms.Compose([
-    #                 transforms.Resize((128, 128)),
-    #                 transforms.ToTensor()
-    #     ])
-    #     img_tensor = transform(img).unsqueeze(0)
-
-    #     with torch.no_grad():
-    #         img_tensor = img_tensor.to(device)
-    #         prediction = model(img_tensor)
-    #         probabilities = torch.nn.functional.softmax(prediction, dim = 1)
-    #         pred_idx = probabilities.argmax(dim=1).item()
-
-    #         class_to_index = {v: k for k, v in ImageFolder("dictionaries/letters/train/").class_to_idx.items()}
-    #         class_names = class_to_index[pred_idx]
-    #         print(class_names)
+    evaluator.make_string()
