a302fb94ebddabd40acff9c633a09c7469957f3c
Filip Dravecký <dravefil@fit.cvut.cz>
2025-12-15 11:36:25 +0100

    Tool: new utility is_blank

--- tools/utils.py
+++ tools/utils.py
@@ -1,9 +1,18 @@
 """
 helper functions
 """
+import PIL
+
 
 def percetage(number, percent) -> int:
     """
     calculates given percetange of given number
     """
     return number * percent / 100
+
+def is_blank(image, background = (255, 255, 255)) -> bool:
+    """
+    Checks if given image is blank
+    """
+    colors = image.getcolors()
+    return len(colors) == 1 and colors[0][1] == background
