91d530af905a429deeaed37b06b0c6e008320bb9
Filip Dravecký <dravefil@fit.cvut.cz>
2025-12-15 14:51:54 +0100

    Tool: implemented Slider class

--- scenes/level.py
+++ scenes/level.py
@@ -7,6 +7,7 @@ import tools.table_generator as tg
 import tools.brush as brush
 import tools.canvas as canvas
 import tools.attempts as attempts
+from tools.slider import CSlider as Slider
 from tools.evaluator import CEvaluator
 
 SCREEN_W = 1024 # 4 x 3
@@ -30,6 +31,25 @@ class CLevel:
         self.__brush = brush.CBrush()
         self.__evaluator = CEvaluator()
 
+        self.__center_collider = Slider(    self.__screen, percetage(SCREEN_W, 10)  ,
+                                            SCREEN_H - percetage(SCREEN_H, 5)       ,
+                                            SCREEN_W - 2 * percetage(SCREEN_W, 10)  ,
+                                            percetage(SCREEN_H, 5)                  ,
+                                            active = True
+                                        )
+        self.__left_collider = Slider  (    self.__screen,   0      ,
+                                            0                       ,
+                                            percetage(SCREEN_W, 5)  ,
+                                            SCREEN_H                ,
+                                            active = True
+                                        )
+        self.__right_collider = Slider (    self.__screen,  SCREEN_W - percetage(SCREEN_W, 5) ,
+                                            0                                                 ,
+                                            percetage(SCREEN_W, 5)                            ,
+                                            SCREEN_H                                          ,
+                                            active = True
+                                        )
+
     def display_level(self):
         """
         Metod called in game loop
@@ -41,27 +61,12 @@ class CLevel:
         self.__canvases.display_canvases(self.__screen, self.__x_pos, self.__y_pos)
         self.__attempts.display_attempts(self.__x_pos, self.__y_pos)
 
-        center_collider  = pygame.Rect  (   percetage(SCREEN_W, 10) + self.__x_pos            ,
-                                            SCREEN_H - percetage(SCREEN_H, 5) + self.__y_pos  ,
-                                            SCREEN_W - 2 * percetage(SCREEN_W, 10)            ,
-                                            percetage(SCREEN_H, 5)
-                                            )
-        left_collider    = pygame.Rect  (   0 + self.__x_pos        ,
-                                            0 + self.__y_pos        ,
-                                            percetage(SCREEN_W, 5)  ,
-                                            SCREEN_H
-                                            )
-        right_collider   = pygame.Rect  (   SCREEN_W - percetage(SCREEN_W, 5) + self.__x_pos ,
-                                            0 + self.__y_pos                                 ,
-                                            percetage(SCREEN_W, 5)                           ,
-                                            SCREEN_H
-                                            )
 
-        pygame.draw.rect(self.__screen, (255,255,255),center_collider)
-        pygame.draw.rect(self.__screen, (255,255,255),left_collider)
-        pygame.draw.rect(self.__screen, (255,255,255),right_collider)
+        self.__center_collider.display_slider(self.__x_pos, self.__y_pos)
+        self.__left_collider.display_slider(self.__x_pos, self.__y_pos)
+        self.__right_collider.display_slider(self.__x_pos, self.__y_pos)
 
-        continue_looping = self.__event_handler(center_collider, left_collider, right_collider)
+        continue_looping = self.__event_handler(self.__x_pos, self.__y_pos)
 
 
         self.__x_pos += self.__x_off
@@ -79,7 +84,7 @@ class CLevel:
 
         return continue_looping
 
-    def __event_handler(self, center_collider, left_collider, right_collider):
+    def __event_handler(self, global_x, global_y):
         """
         Metod handling all inputs and events
         """
@@ -91,17 +96,17 @@ class CLevel:
                     self.__brush.down()
                     # last_pos = event.pos
 
-                if center_collider.collidepoint(event.pos):
+                if self.__center_collider.collidepoint(global_x, global_y, event.pos):
                     if self.__y_pos >= 0:
                         self.__y_off = -8
                     else:
                         self.__y_off = 8
-                elif right_collider.collidepoint(event.pos):
+                elif self.__right_collider.collidepoint(global_x, global_y, event.pos):
                     if self.__x_pos >= 0:
                         self.__x_off = -8
                     else:
                         self.__x_off = 8
-                elif left_collider.collidepoint(event.pos):
+                elif self.__left_collider.collidepoint(global_x, global_y, event.pos):
                     if self.__x_pos <= 0:
                         self.__x_off = 8
                     else:
