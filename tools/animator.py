910a1895dff1e44880eb1236e3d849d7b848df9e
Filip Dravecký <dravefil@fit.cvut.cz>
2025-12-17 17:08:35 +0100

    Tool: added animator tool

---
+++ tools/animator.py
@@ -0,0 +1,44 @@
+"""
+Module for animation handling
+"""
+import os
+import pygame
+
+class CAnimations:
+    """
+    Class for loading sprites into 
+    animation and playing it
+    """
+
+    def __init__(self, path, fps = 30):
+        directory     = os.listdir(path)
+        self.__am_frames     = len(directory)
+
+        self.__frames = [pygame.image.load(path + "/" + frame) for frame in directory]
+
+        self.__frame_index = 0
+        self.__timer       = 0
+        self.__frame_time  = fps / self.__am_frames
+
+    def rescale(self, transform):
+        """
+        Rescales all frames of animation
+        """
+        self.__frames = [pygame.transform.scale(frame, transform) for frame in self.__frames]
+
+    def play(self, screen : pygame.Surface, delta_time, cordinates : tuple):
+        """
+        Plays animation in scene
+        """
+        self.__timer += delta_time
+
+        if self.__timer >= self.__frame_time:
+            self.__timer -= self.__frame_time
+            self.__frame_index = (self.__frame_index + 1) % self.__am_frames
+
+        screen.blit(self.__frames[self.__frame_index], cordinates)
+
+
+
+if __name__ == "__main__":
+    animator = CAnimations("assets/dripping")
