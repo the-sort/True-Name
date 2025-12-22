a2f7c4977a6854b305c18039922cb5f41ddd4a9c
Filip Dravecký <dravefil@fit.cvut.cz>
2025-12-22 03:21:22 +0100

    Menu: added idling wizzard to scene

--- scenes/menu.py
+++ scenes/menu.py
@@ -2,6 +2,7 @@
 Script that generates menu
 """
 import pygame
+from tools.anim_button import CAnimButton, CAnimations
 
 SCREEN_W = 1024 # 4 x 3
 SCREEN_H = 768
@@ -12,8 +13,11 @@ class CMenu:
     """
     def __init__(self):
         self.__screen = pygame.display.set_mode((SCREEN_W, SCREEN_H))
+        self.__wizzad = CAnimButton(pos = (SCREEN_W //2, SCREEN_H//2),
+                                    idle = CAnimations("assets/wiz_idle", fps = 5),
+                                    action = test)
 
-    def display_menu(self):
+    def display_menu(self, delta_time):
         """
         Method for displaying menu,
         intended to be called in 
@@ -21,6 +25,7 @@ class CMenu:
         """
         self.__screen.fill((0,0,0))
         continue_looping = self.handle_events()
+        self.__wizzad.display(self.__screen, 0, 0, delta_time)
 
         return continue_looping
 
@@ -33,8 +38,8 @@ class CMenu:
                 return False
         return True
 
-
-
+def test():
+    print("Hello world")
 
 if __name__ == "__main__":
     RUNNING  = True
@@ -47,7 +52,7 @@ if __name__ == "__main__":
 
     while RUNNING:
 
-        RUNNING = menu.display_menu()
+        RUNNING = menu.display_menu(DELTA_TIME)
 
         pygame.display.flip()
 
