31dd0e20310f03a82c856d387ef16dc10b725704
Filip Dravecký <dravefil@fit.cvut.cz>
2025-12-24 16:06:18 +0100

    Menu: Easy contract fully implemented

--- scenes/menu.py
+++ scenes/menu.py
@@ -4,6 +4,8 @@ Script that generates menu
 import pygame
 from tools.anim_button import CAnimButton, CAnimations
 from tools.button      import CButton
+from scenes.scene_manager import CSceneManager
+from scenes.level         import CLevel
 
 SCREEN_W = 1024 # 4 x 3
 SCREEN_H = 768
@@ -12,16 +14,22 @@ SCREEN_H = 768
 #WIZ_HOVER 500x500
 
 
-class CMenu:
+class CMenu():
     """
     Class for handling menu
     """
-    def __init__(self):
+    def __init__(self, manager : CSceneManager):
+        self.__manager = manager
+
         self.__screen = pygame.display.set_mode((SCREEN_W, SCREEN_H))
 
         temp_pos = (0,0)
+        off_set  = 50
         self.__easy     = CButton(  position = temp_pos,
-                                    idle = "assets/difficultie_posters/EASY_idle.png")
+                                    idle = "assets/difficultie_posters/EASY_idle.png",
+                                    pressed = "assets/difficultie_posters/EASY_pressed.png",
+                                    on_press = lambda : self.inspect_contact(self.__easy),
+                                )
         self.__medium   = CButton(  position = temp_pos,
                                     idle = "assets/difficultie_posters/MEDIUM_idle.png")
         self.__hard   = CButton(    position = temp_pos,
@@ -34,17 +42,37 @@ class CMenu:
                                     on_press = self.start_shoping,
                                     on_release= self.exit_shoping)
 
+
+        self.__decline = CButton(   position = temp_pos,
+                                    idle = "assets/decline.png",
+                                    active = False,
+                                    on_press = self.refuse_contract,
+                                    on_release= self.refuse_contract,
+                                    visible = False
+                                )
+        self.__accept = CButton(   position= temp_pos,
+                                    idle= "assets/accept.png",
+                                    active = False,
+                                    on_press = self.accept_contract,
+                                    on_release= self.accept_contract,
+                                    visible = False
+                                )
+
         self.__easy.move((250, SCREEN_H - 1.5 * self.__easy.height()))
         self.__medium.move((450, SCREEN_H - 2 * self.__medium.height()))
 
-        temp_pos = self.__center_align(self.__hard.get_idle())
+        temp_pos = self.__center_align((self.__hard.width(), self.__hard.height()))
         self.__hard.move((temp_pos[0], 100))
-
-        self.__board = pygame.image.load("assets/bounty_board.png")
+        self.__decline.move((SCREEN_W//2 - self.__decline.width(), SCREEN_H - self.__decline.height() - off_set))
+        self.__accept.move(((SCREEN_W//2, SCREEN_H - self.__accept.height() - off_set)))
 
         self.__wizzad.right_bottom((SCREEN_W, SCREEN_H))
 
-    def display_menu(self, delta_time):
+        self.__board = pygame.image.load("assets/bounty_board.png")
+
+        self.__orig_pos = (0, 0)
+
+    def display(self, delta_time):
         """
         Method for displaying menu,
         intended to be called in 
@@ -53,14 +81,17 @@ class CMenu:
         self.__screen.fill((0,0,0))
         continue_looping = self.handle_events()
 
-        self.__screen.blit(self.__board, self.__center_align(self.__board))
+        self.__screen.blit(self.__board, self.__center_align((self.__board.get_width(), self.__board.get_height())))
+
+        self.__wizzad.display(self.__screen, 0, 0, delta_time)
 
         self.__easy.display(self.__screen, 0, 0)
         self.__medium.display(self.__screen, 0, 0)
         self.__hard.display(self.__screen, 0, 0)
 
-        self.__wizzad.display(self.__screen, 0, 0, delta_time)
 
+        self.__decline.display(self.__screen, 0, 0)
+        self.__accept.display(self.__screen, 0, 0)
         return continue_looping
 
     def handle_events(self) -> bool:
@@ -84,13 +115,64 @@ class CMenu:
         """
         self.__wizzad.right_bottom((SCREEN_W, SCREEN_H))
 
-    def __center_align(self, surf : pygame.Surface):
+    def inspect_contact(self, contract : CButton):
+        """
+        Moves contract to center of screen
+        """
+        pos = self.__center_align((contract.width("pressed"), contract.height("pressed")))
+        self.__orig_pos = contract.postion()
+        contract.move(pos)
+
+        self.__call_on_buttons("deactivate", [self.__easy, self.__medium, self.__hard, self.__wizzad])
+        self.__call_on_buttons("hide", [self.__easy, self.__medium, self.__hard], [contract])
+        self.__call_on_buttons("show", [self.__accept, self.__decline])
+        self.__call_on_buttons("activate", [self.__accept, self.__decline])
+
+        self.__active_contract = contract
+
+    def refuse_contract(self):
+        """
+        Moves contract to original postion
+        """
+        self.__active_contract.move(self.__orig_pos)
+        self.__active_contract.make_idle()
+
+        self.__call_on_buttons("activate", [self.__easy, self.__medium, self.__hard, self.__wizzad])
+        self.__call_on_buttons("show", [self.__easy, self.__medium, self.__hard])
+        self.__call_on_buttons("hide", [self.__accept, self.__decline])
+        self.__call_on_buttons("deactivate", [self.__accept, self.__decline])
+
+    def accept_contract(self):
+        """
+        Changes Scene to desired 
+        level
+        """
+        match self.__active_contract:
+            case self.__easy:
+                self.__manager.change_scene(CLevel("EASY"))
+            case self.__medium:
+                self.__manager.change_scene(CLevel("MEDIUM"))
+            case self.__hard:
+                self.__manager.change_scene(CLevel("HARD"))
+
+
+
+    def __center_align(self, size):
         """
         find center cordinates for given surf
         """
-        size = surf.get_rect().size
         pos = (SCREEN_W // 2 - size[0]//2, SCREEN_H // 2 - size[1]//2)
         return pos
+
+    def __call_on_buttons(self, method, buttons, skip = None):
+        """
+        Iterates throught buttons and
+        hides them
+        """
+        for button in buttons:
+            if skip is not None and button in skip:
+                continue
+            getattr(button, method)()
 
 
 if __name__ == "__main__":
@@ -100,11 +182,11 @@ if __name__ == "__main__":
     pygame.init()
     clock = pygame.time.Clock()
 
-    menu = CMenu()
+    menu = CMenu(CSceneManager())
 
     while RUNNING:
 
-        RUNNING = menu.display_menu(DELTA_TIME)
+        RUNNING = menu.display(DELTA_TIME)
 
         pygame.display.flip()
 
