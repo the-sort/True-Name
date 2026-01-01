cb365727bbbe5bff8365062642f80b299c12b8b1
Filip Dravecký <dravefil@fit.cvut.cz>
2026-01-01 22:47:17 +0100

    Shop: Initial shop implementation

---
+++ tools/shop.py
@@ -0,0 +1,87 @@
+"""
+Tool for displaying and handling shop
+"""
+import pygame
+from tools.button import CButton
+
+class CShop:
+    """
+    Class for displaying and handling shop
+    """
+    def __init__(self, screen_w, screen_h):
+        temp_pos = (0, 0)
+
+        self.__screen_w = screen_w
+        self.__screen_h = screen_h
+
+        self.__shop_frame = pygame.image.load("assets/shop_frame.png")
+        self.__coin_img = pygame.image.load("assets/coin_scaled.png")
+        self.__health_img = pygame.image.load("assets/health_vial.png")
+
+        self.__small_coin = pygame.transform.scale(self.__coin_img, (35, 35))
+
+        self.__plus = CButton(  position    = temp_pos,
+                                idle        = "assets/plus_button.png",
+                                active      = False,
+                                visible     = True
+                             )
+        
+
+
+    def display(self, screen, manager):
+        """
+        Method for displaying shop
+        """
+        font_size = 25
+        off_set   = 50
+        prize = 500
+
+        font = pygame.font.Font("./dictionaries/Gothic_pixel_font_fixed.ttf", font_size)
+        coins = font.render(str(manager.player.coins()), True, (255, 255, 255))
+        font = pygame.font.Font("./dictionaries/Gothic_pixel_font_fixed.ttf", 17)
+        health = font.render(str(manager.player.health())+"/1000", True, (255, 255, 255))
+        prize_txt = font.render(str(prize), True, (255, 255, 255))
+
+        shop_frame_size = self.__shop_frame.get_rect().size
+        coins_size = coins.get_rect().size
+        coin_img_size = self.__coin_img.get_rect().size
+        header_size = (coins_size[0] + coin_img_size[0], max(coins_size[1], coin_img_size[1]))
+        health_size = health.get_rect().size
+        prize_txt_size = prize_txt.get_rect().size
+        
+        shop_frame_top_left = (0, self.__screen_h // 2 - self.__shop_frame.get_rect().height // 2)
+        coins_am_top_left = (shop_frame_top_left[0] + shop_frame_size[0] // 2 - header_size[0] // 2, shop_frame_top_left[1] + header_size[1])
+        health_top_left = (shop_frame_top_left[0] + off_set, coins_am_top_left[1] + 2 * off_set)
+        health_img_top_left = (health_top_left[0] + health_size[0], health_top_left[1])
+
+
+        shop_frame_top_right = (shop_frame_size[0], shop_frame_top_left[1])
+        health_top_right = (shop_frame_top_right[0], health_top_left[1])
+
+        small_coin_top_left = (health_top_right[0] - self.__plus.width() - off_set, health_top_right[1])
+        prize_top_left = (small_coin_top_left[0] - prize_txt_size[0] - off_set // 5, small_coin_top_left[1])
+        plus_top_left = (prize_top_left[0] - self.__plus.width() - off_set // 5, small_coin_top_left[1])
+
+        self.__plus.move(plus_top_left)
+
+        screen.blit(self.__shop_frame, shop_frame_top_left)
+        screen.blit(coins, coins_am_top_left)
+        screen.blit(self.__coin_img, (coins_am_top_left[0] + coins.get_width() + off_set, coins_am_top_left[1]))
+        screen.blit(health, health_top_left)
+        screen.blit(self.__health_img, health_img_top_left)
+        screen.blit(prize_txt, prize_top_left)
+        screen.blit(self.__small_coin, small_coin_top_left)
+        
+        self.__plus.display(screen, 0, 0)
+
+    # def __render_font(self, font_size):
+    #     """
+    #     Returns rendered font
+    #     """
+    #     font = pygame.font.Font("./dictionaries/Gothic_pixel_font_fixed.ttf", font_size)
+
+
+
+
+if __name__ == "__main__":
+    pass
