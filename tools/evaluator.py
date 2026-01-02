2f011121497fcb9ebb2fef72888861c449a81a5a
Filip Dravecký <dravefil@fit.cvut.cz>
2026-01-02 19:02:55 +0100

    Tool: Evaluator PEP-8 styling

--- tools/evaluator.py
+++ tools/evaluator.py
@@ -106,8 +106,11 @@ class CEvaluator:
         color   = (255, 255, 255)
         font = pygame.font.Font("./dictionaries/Gothic_pixel_font_fixed.ttf", font_size)
         guess_surface = font.render(self.__guess, True, color)
-        screen.blit(guess_surface, ((screen.get_width()//2) - (guess_surface.get_width()/2) + global_x, 
-                                    screen.get_height() + SLIDER_H  -guess_surface.get_height()//1.5 + global_y))
+        guess_pos = (
+                        (screen.get_width() // 2) - (guess_surface.get_width()/2) + global_x,
+                        screen.get_height() + SLIDER_H  - guess_surface.get_height()//1.5 + global_y
+                    )
+        screen.blit (guess_surface, guess_pos)
 
 
 
